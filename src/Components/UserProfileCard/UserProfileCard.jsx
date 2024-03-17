ebc1af71aa98ea36e7201caa6c377c1b5e9e3dbf
Jackie Jr <jwysinger@gmail.com>
2024-03-17 10:27:49 -0500

    created base of app

---
+++ src/Components/UserProfileCard/UserProfileCard.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './UserProfileCard.css'
+
+const UserProfileCard = () => {
+  return (
+    <div className='upc'>
+      <div className='gradient'></div>
+      <div className='profile-down'>
+        <img src=''/>
+        <div className='profile-title'></div>
+        <div className='profile-description'>
+
+        </div>
+        <div className='profile-button'><a href='mailto:'></a></div>
+      </div>
+    </div>
+  )
+}
+
+export default UserProfileCard
