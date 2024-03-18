e7e30c2f33e5b74db8f818f108618dd0bbb9d167
Jackie Jr <jwysinger@gmail.com>
2024-03-18 12:09:03 -0500

    css for profile card

--- src/Components/UserProfileCard/UserProfileCard.jsx
+++ src/Components/UserProfileCard/UserProfileCard.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
 import './UserProfileCard.css'
+import profile_icon from '/Users/jackiejr/Desktop/Web Design/react-profile-card/src/Components/Assets/IMG_8473.jpg'
 
 const UserProfileCard = () => {
   return (
     <div className='upc'>
       <div className='gradient'></div>
       <div className='profile-down'>
-        <img src=''/>
-        <div className='profile-title'></div>
+        <img src={profile_icon}/>
+        <div className='profile-title'>Jackie Wysinger</div>
         <div className='profile-description'>
-
+          I am Jackie Wysinger, Freelance Web Developer and Graphic Designer. 
         </div>
-        <div className='profile-button'><a href='mailto:'></a></div>
+        <div className='profile-button'><a href='mailto:jwysinger@gmail.com'>Contact Me</a></div>
       </div>
     </div>
   )
